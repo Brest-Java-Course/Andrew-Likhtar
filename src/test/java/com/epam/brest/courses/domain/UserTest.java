41fe600d0f2e9e599e7530c13e30b86c58dc16f5
artificiallight92 <artificiallight92@gmail.com>
2014-10-17 20:36:12 +0300

    test

---
+++ src/test/java/com/epam/brest/courses/domain/UserTest.java
@@ -0,0 +1,13 @@
+package com.epam.brest.courses.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    @Test
+    public void testGetUserName() throws Exception {
+
+    }
+}
